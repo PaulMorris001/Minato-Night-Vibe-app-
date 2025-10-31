6b876ae5b0e0dc92dfa32b09a8e41e9473f83128
Setemi Loye <setemiloye@gmail.com>
2025-10-31 01:08:06 +0100

    Login/signup almost complete

---
+++ server/src/index.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import connectDB from './config/db.js';
+
+import authRoutes from './routes/auth.route.js'
+
+dotenv.config();
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+connectDB();
+
+app.use("/api/", authRoutes)
+
+app.listen(process.env.PORT, ()=>{console.log("Backend started at port: ", process.env.PORT)})
