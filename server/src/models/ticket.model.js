7ed734cc3bbbdd6932ad1346e8a5bdc7a7adf479
Setemi Loye <setemiloye@gmail.com>
2025-12-04 19:24:22 +0100

    buying ticket patch

--- server/src/models/ticket.model.js
+++ server/src/models/ticket.model.js
@@ -23,7 +23,7 @@ const ticketSchema = mongoose.Schema({
   isValid: { type: Boolean, default: true },
 
   // Unique ticket code for verification
-  ticketCode: { type: String, unique: true, required: true }
+  ticketCode: { type: String, unique: true, sparse: true }
 }, {
   timestamps: true
 });
